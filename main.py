48b4ba541b79a81a69b3cd8c8f39ce373131d674
ruthcaswellsmith <ruth@plannuh.com>
2022-04-02 06:52:31 -0400

    Added self.b

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 class Test:
     def __init__(self):
-         self.a = 4
+        self.a = 4
+        self.b = 10
 
 if __name__ == "__main__":
 
     test = Test()
-    print(test.a)
+    print(test.a, test.b)
