6959dd60f1fc483a5b81f9d6a130ddbbe4dd5043
ruthcaswellsmith <ruth@plannuh.com>
2022-04-02 06:41:34 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#  This is a simple python script to test git workflow
+
+class Test:
+    def __init__(self):
+         self.b = 4
+
+if __name__ == "__main__":
+
+    test = Test()
+    print(test.a)
