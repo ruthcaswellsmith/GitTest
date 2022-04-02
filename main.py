84cdfb6413a9aa8fbed9fe6a4b1425f3216a095e
ruthcaswellsmith <ruth@plannuh.com>
2022-04-02 15:00:11 -0400

    Added Feature 2

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ class Test:
         self.b = 10
 
 
+class Feature2:
+    def __init__(self):
+        pass
+
+    @property
+    def name(self):
+        return 'Feature 2'
+
+
 if __name__ == "__main__":
 
     test = Test()
     print(test.a, test.b)
+
+    feature2 = Feature2()
+    print(feature2.name)
